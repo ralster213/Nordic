d6d13c54b4e27dd4f6e31294bec56c67e8c685fd
EthanKoland <ethankoland@gmail.com>
2020-04-08 14:25:06 -0500

    test GitHub

---
+++ NordicPractice/cloudControll.swift
@@ -0,0 +1,16 @@
+//
+//  cloudControll.swift
+//  NordicPractice
+//
+//  Created by 90305076 on 4/8/20.
+//  Copyright © 2020 90305007. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+
+class Captain {
+    name = ""
+    
+}
